16db4f99cb3255baf37c565d5ed6b801b52b73fb
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2023-05-04 17:47:13 +0000

    mvc

--- classe.php
+++ classe.php
@@ -5,23 +5,193 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestion des Classes</title>
-    <link rel="stylesheet" href="View/style.css">
+    <title>Gestion des Etudiants</title>
+    <!-- ======= Styles ====== -->
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
+    <!-- =============== Navigation ================ -->
     <div class="container">
-        <a href="insert.php" class="Btn_add"> <img src="View/images/plus.png"> Ajouter Etudiant</a>
-        <a href="insertclasse.php" class="Btn_add"> <img src="View/images/plus.png">Ajouter une Classe</a>
+        <div class="navigation">
+            <ul>
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="logo-apple"></ion-icon>
+                        </span>
+                        <span class="title">AppGestSchool</span>
+                    </a>
+                </li>
 
-        <table>
-            <tr id="items">
-                <th>Classe</th>
-                <th>Nombre d'etudiant</th>
-                <th>Modifier</th>
-                <th>Supprimer</th>
-            </tr>
-            <?php
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="home-outline"></ion-icon>
+                        </span>
+                        <span class="title">Dashboard</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="insert.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Add Students</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="index.php">
+                        <span class="icon">
+                            <ion-icon name="people-outline"></ion-icon>
+                        </span>
+                        <span class="title">Students</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="classe.php">
+                        <span class="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span class="title">Classes</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="insertclasse.php">
+                        <span class="icon">
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                        </span>
+                        <span class="title">Add Classes</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="help-outline"></ion-icon>
+                        </span>
+                        <span class="title">Help</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="settings-outline"></ion-icon>
+                        </span>
+                        <span class="title">Settings</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="lock-closed-outline"></ion-icon>
+                        </span>
+                        <span class="title">Password</span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#">
+                        <span class="icon">
+                            <ion-icon name="log-out-outline"></ion-icon>
+                        </span>
+                        <span class="title">Sign Out</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- ========================= Main ==================== -->
+        <div class="main">
+            <div class="topbar">
+                <div class="toggle">
+                    <ion-icon name="menu-outline"></ion-icon>
+                </div>
+
+                <div class="search">
+                    <label>
+                        <input type="text" placeholder="Recherche">
+                        </ion-icon>
+                    </label>
+                </div>
+
+                <div class="user">
+                    <img src="assets/imgs/customer01.jpg" alt="">
+                </div>
+            </div>
+
+            <!-- ======================= Cards ================== -->
+            <div class="cardBox">
+                <div class="card">
+                    <div>
+                        <div class="numbers">1,504</div>
+                        <div class="cardName">Etudiants</div>
+                    </div>
+
+                    <div class="iconBx">
+                        <ion-icon name="person"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="numbers">4</div>
+                        <div class="cardName">Batiments</div>
+                    </div>
+
+                    <div class="iconBx">
+                        <ion-icon name="cart-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="numbers">20</div>
+                        <div class="cardName">Classes</div>
+                    </div>
+
+                    <div class="iconBx">
+                        <ion-icon name="chatbubbles-outline"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="card">
+                    <div>
+                        <div class="numbers">75</div>
+                        <div class="cardName">Salles</div>
+                    </div>
+
+                    <div class="iconBx">
+                        <ion-icon name="cash-outline"></ion-icon>
+                    </div>
+                </div>
+            </div>
+
+            <!-- ================ Order Details List ================= -->
+            <div class="details">
+                <div class="recentOrders">
+                    <div class="cardHeader">
+                        <h2>Listes Des Classes</h2>
+                        <a href="#" class="btn">View All</a>
+                    </div>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Classe</td>
+                                <td>Nombre Etudiant</td>
+                                <td>Modifier</td>
+                                <td>Supprimer</td>
+                            </tr>
+                        </thead>
+
+                            <?php
             //inclure la page de connexion
             include_once "connectdb.php";
             //requête pour afficher la liste des etudiants
@@ -33,27 +203,124 @@
                 //si non , affichons la liste de tous les etudiants
                 while ($row = mysqli_fetch_assoc($req)) {
             ?>
+           
+                <tbody>
                     <tr>
                         <td><?= $row['nom'] ?></td>
                         <td></td>
 
-
                         <!--Nous alons mettre l'id de chaque etudiants dans ce lien -->
-                        <td><a href=""><img src="View/images/pen.png"></a></td>
-                        <td><a href=""><img src="View/images/trash.webp"></a></td>
+                        <td><span class="status delivered"><a href="updateclasse.php?id=<?= $row['id'] ?>">Update</a></span></td>
+                        <td><span class="status delete"><a href="deleteclasse.php?id=<?= $row['id'] ?>">Delete</a></span></td>
                     </tr>
+                    
             <?php
                 }
             }
             ?>
+                    </table>
+                </div>
 
+                <!-- ================= New Customers ================ -->
+                <div class="recentCustomers">
+                    <div class="cardHeader">
+                        <h2>Recent Customers</h2>
+                    </div>
 
-        </table>
+                    <table>
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Abdallah <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
 
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Mohamed <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
 
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Abdallah <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
 
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Mohamed <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
 
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Abdallah <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Mohamed <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer01.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Abdallah <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td width="60px">
+                                <div class="imgBx"><img src="assets/imgs/customer02.jpg" alt=""></div>
+                            </td>
+                            <td>
+                                <h4>Mohamed <br> <span>Senegal</span></h4>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <!-- =========== Scripts =========  -->
+    <script src="assets/js/main.js"></script>
+
+    <!-- ====== ionicons ======= -->
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 </body>
 
 </html>
+
+
+
+
+
+
+
+
+
+
